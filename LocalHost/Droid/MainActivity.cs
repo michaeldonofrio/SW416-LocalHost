3d77b6027b608acd99648608ae2d63d5a058397f
Daniel Vasconez <daniel.vasconez@student.fairfield.edu>
2018-04-11 16:26:59 -0400

    Added App Icon

--- LocalHost/Droid/MainActivity.cs
+++ LocalHost/Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 
 namespace LocalHost.Droid
 {
-    [Activity(Label = "LocalHost", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "LocalHost", Icon = "@drawable/LocalHostLogo", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
