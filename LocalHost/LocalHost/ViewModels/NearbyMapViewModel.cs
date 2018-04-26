a9f55c9060c37463014c76948604ca320b9947ec
Michael Donofrio <michael.a.donofrio@hotmail.com>
2018-04-26 15:21:31 -0400

    Added NearbyMap View and View Model

---
+++ LocalHost/LocalHost/ViewModels/NearbyMapViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace LocalHost.ViewModels
+{
+    public class NearbyMapViewModel : ViewModelBase
+    {
+        public NearbyMapViewModel(Page page) : base(page)
+        {
+        }
+    }
+}
---
+++ LocalHost/LocalHost/Views/NearbyMapPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using LocalHost.ViewModels;
+using Xamarin.Forms;
+
+namespace LocalHost.Views
+{
+    public partial class NearbyMapPage : ContentPage
+    {
+        NearbyMapViewModel viewModel;
+        public NearbyMapPage()
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new NearbyMapViewModel(this);
+        }
+    }
+}
