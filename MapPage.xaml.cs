6aa1e4d04b1abc16b4aeb16a3cec6cefe0f859e4
PeterTJulian <31773909+PeterTJulian@users.noreply.github.com>
2018-04-27 19:34:44 -0400

    Add files via upload
    Lots of map stuff. Working API key. App crashes on startup though

---
+++ MapPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using LocalHost.Models;
+using LocalHost.ViewModels;
+using Plugin.Geolocator;
+using Xamarin.Forms;
+
+namespace LocalHost.Views
+{  
+    public partial class MapPage : ContentPage
+    {
+        MapViewModel viewModelMap;
+        public MapPage(){
+            InitializeComponent();
+            BindingContext = viewModelMap = new MapViewModel(null, this);
+        }
+
+       /* void updateUser (object sender, System.EventArgs e)
+        {
+            string updatedUsername = UsernameCell.Text;
+            string updatedName = NameCell.Text;
+            viewModel.updateUser(updatedUsername, updatedName);
+        }*/
+    }
+}
---
+++ MapViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using LocalHost.Models;
+using Plugin.Geolocator;
+using Xamarin.Forms;
+
+namespace LocalHost.ViewModels
+{
+    public class MapViewModel : ViewModelBase, IObserverViewModel
+    {
+        IDataStore DataStore;
+        private User user;
+        public User User { get { return user; }
+                           set { SetProperty(ref user, value); }}
+
+        public MapViewModel(User user, Page page) : base(page)
+        {
+            this.User = user;
+            DataStore = App.dataStore;
+            DataStore.Subscribe(this);
+            getData();
+        }
+
+        /*public void updateUser(string updatedUsername, string updatedName){
+            User.Username = updatedUsername;
+            DataStore.UpdateUser(User);
+        }*/
+
+        public void getData()
+        {
+            User user = DataStore.GetUser().Result;
+            this.User = user;
+        }
+    }
+}
